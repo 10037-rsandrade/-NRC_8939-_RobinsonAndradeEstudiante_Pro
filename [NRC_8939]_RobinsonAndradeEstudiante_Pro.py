873cd2a7e627dfaa258cec27642bef3137e0cb5e
Robinson Steve Andrade Puente <93177069+AndradeRobinson@users.noreply.github.com>
2022-11-23 21:41:46 -0500

    Update [NRC_8939]_RobinsonAndradeEstudiante_Pro.py
    Cambios y actualizacion

--- [NRC_8939]_RobinsonAndradeEstudiante_Pro.py
+++ [NRC_8939]_RobinsonAndradeEstudiante_Pro.py
@@ -1,10 +1,10 @@
-from collections import namedtuple
+from collections import namedtuple #libreria para llamar a las colecciones
 
-Beneficiario=namedtuple('Beneficiario',['cedula','nombres','apellidos','DNI','edad','NSS'])
-Empleado=namedtuple('Empleado',['cedula','nombres','apellidos','DNI','edad','NSS','titulo'])
-Riesgo=namedtuple('Riesgo',['nombre_riesgo','tipo_riesgo'])
-Agencia=namedtuple('Agencia',['nombre_agencia','direccion','telefono','ciudad'])
-Residencia=namedtuple('Residencia',['nombre_residencia','direccion','sector'])
-Categoria=namedtuple('Categoria',['nombre_categoria','tipo_sueldo','sueldo_hora'])
-Prestamo=namedtuple('Prestamo',['codigo','tipo_interes','periodo','vigencia'])
-Fecha=namedtuple('Fecha',['fecha_inicio','fecha_final'])
+Beneficiario=namedtuple('Beneficiario',['cedula','nombres','apellidos','DNI','edad','NSS']) #creacion de la entidad beneficiario
+Empleado=namedtuple('Empleado',['cedula','nombres','apellidos','DNI','edad','NSS','titulo']) #creacion de la entidad Empleado
+Riesgo=namedtuple('Riesgo',['nombre_riesgo','tipo_riesgo']) #creacion de la entidad Riesgo
+Agencia=namedtuple('Agencia',['nombre_agencia','direccion','telefono','ciudad']) #creacion de la entidad Agencia
+Residencia=namedtuple('Residencia',['nombre_residencia','direccion','sector']) #creacion de la entidad Residencia
+Categoria=namedtuple('Categoria',['nombre_categoria','tipo_sueldo','sueldo_hora']) #creacion de la entidad Categoria
+Prestamo=namedtuple('Prestamo',['codigo','tipo_interes','periodo','vigencia']) #creacion de la entidad Prestamo
+Fecha=namedtuple('Fecha',['fecha_inicio','fecha_final']) #creacion de la entidad Fecha
